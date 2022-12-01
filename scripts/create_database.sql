02aad7f537a3fe006ad9685de19f0288123745c3
Adam Leskis <leskis@gmail.com>
2022-12-01 23:36:38 +0000

    add table creation to db init scripts

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE IF NOT EXISTS transactions;
+
+CREATE TABLE IF NOT EXISTS referrals (
+    referral_id mediumint NOT NULL AUTO_INCREMENT,
+    currency varchar(255),
+    email varchar(255),
+    referral_code varchar(255),
+    referral_submitted varchar(255),
+    referree varchar(255),
+    referrer varchar(255),
+    username varchar(255)
+    );
